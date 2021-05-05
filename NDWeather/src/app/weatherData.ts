2ff40eb5f38450e8efea3094eb239fac3e0e1168
jacksont432 <tommy.jackson@ndsu.edu>
2021-05-05 13:24:58 -0500

    a

--- NDWeather/src/app/weatherData.ts
+++ NDWeather/src/app/weatherData.ts
@@ -1,5 +1,32 @@
 import { Weather } from './weather';
 
 export const fargoData: Weather[] = [
-    
+
 ]
+
+export const bismarckData: Weather[] = [
+
+]
+
+export const grandForksData: Weather[] = [
+
+]
+
+export const minotData: Weather[] = [
+
+]
+
+export const willistonData: Weather[] = [
+
+]
+
+export const dickinsonData: Weather[] = [
+
+]
+
+export const JamestownData: Weather[] = [
+
+]
+
+
+
